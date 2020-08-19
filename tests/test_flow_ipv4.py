c1d9aeab457aaedfdcb4a52f9af01e9042d89d75
Andy Balogh <andy.balogh@keysight.com>
2020-08-18 17:22:06 -0700

    export json view

--- tests/test_flow_ipv4.py
+++ tests/test_flow_ipv4.py
@@ -6,28 +6,28 @@ from abstract_open_traffic_generator.flow_ipv4 import Priority, Dscp
 def test_ipv4_priority_dscp(serializer):
     dscp = Dscp(phb=NumberPattern(Dscp.PHB_CS6))
     dscp_priority = Priority(dscp)
-    print(serializer.yaml(dscp_priority))
+    print(serializer.json(dscp_priority))
 
 
 def test_ipv4_priority_dscp_ecn(serializer):
     dscp = Dscp(phb=NumberPattern(Dscp.PHB_CS7), 
         ecn=NumberPattern(Dscp.ECN_CAPABLE_TRANSPORT_1))
     dscp_priority = Priority(dscp)
-    print(serializer.yaml(dscp_priority))
+    print(serializer.json(dscp_priority))
 
 
 def test_ipv4_priority_raw(serializer):
     counter = NumberCounter(start=1, step=1, direction='increment', count=4)
     pattern = NumberPattern(counter)
     raw_priority = Priority(pattern)
-    print(serializer.yaml(raw_priority))
+    print(serializer.json(raw_priority))
 
 
 def test_ipv4(serializer):
     src = StringPattern(StringCounter(start='1.1.1.1', step='0.0.0.1', direction='increment', count=10))
     dst = StringPattern(StringCounter(start='1.1.2.1', step='0.0.0.1', direction='increment', count=10))
-    ipv4 = Ipv4(src=src, dst=dst, priority=Priority(NumberPattern(1)))
-    print(serializer.yaml(ipv4))
+    ipv4 = Ipv4(src=src, dst=dst, priority=Priority(NumberPattern(Priority.PRIORITY_RAW)))
+    print(serializer.json(ipv4))
 
 
 if __name__ == '__main__':
