8bdae9539c95c0d2cdd5f4b108bb6d17c611dcdb
Andy Balogh <andy.balogh@keysight.com>
2020-08-18 18:08:40 -0700

    first port traffic test

---
+++ tests/test_device_traffic.py
@@ -0,0 +1,25 @@
+import pytest
+from abstract_open_traffic_generator.state import Config
+from abstract_open_traffic_generator.flow import *
+from abstract_open_traffic_generator.flow_ipv4 import Priority, Dscp
+from abstract_open_traffic_generator.port import *
+
+
+def test_device_traffic():
+    # ipv41 = Ipv4Device('West Ip', address='10.0.0.1', prefix=16, gateway='10.0.1.1')
+    # eth1 = EthernetDevice('West Eth', children=[ipv41], mac='000000000001')
+    # dev1 = Device('East Devices', sessions_per_port=1, protocols=[eth1])
+    # ipv42 = Ipv4Device('East Ip', address='10.0.1.1', prefix=16, gateway='10.0.0.1')
+    # eth2 = EthernetDevice('East Eth', children=[ipv42], mac='000000000002')
+    # ethernet = Ethernet()
+    # device = Device(name='Device1', parent=None, sessions_per_port=10, 
+    #     protocols=[ethernet, vlan, ipv4])
+    # tx = Port(name='Tx Port', location='1.1.1.1/1/1')
+    # rx = Port(name='Rx Port', location='1.1.1.1/1/2')
+    # device_group = DeviceGroup(name='DeviceGroup1', ports=[tx], 
+    #     devices=[device])
+    pass
+
+
+if __name__ == '__main__':
+    pytest.main(['-s', __file__])
--- tests/test_port_traffic.py
+++ tests/test_port_traffic.py
@@ -1,38 +1,28 @@
 import pytest
+from abstract_open_traffic_generator.state import Config
 from abstract_open_traffic_generator.flow import *
 from abstract_open_traffic_generator.flow_ipv4 import Priority, Dscp
 from abstract_open_traffic_generator.port import *
 
 
-def test_port_traffic(b2b_ports):
-    # priority = Priority(Dscp(phb=0))
-    # ipv4 = Ipv4(src='0.0.0.1', dst='0.0.0.2', 
-    #     priority=Ipv4.Dscp(phb=Dscp.PHB_EF46, ecn=Dscp.ECN_CAPABLE_10))
-    # background = Flow(name='Background Traffic', 
-    #     endpoint=PortEndpoint(tx=tx.name),
-    #     packet=[Ethernet(), Vlan(), ipv4])
-    # pfc = Flow(name='Pfc Traffic', endpoint=PortEndpoint(tx=tx.name),
-    #     packet=[PfcPause()])
-    # config = Config(ports=[tx, rx], flows=[background, pfc])
-    # print(json.dumps(config, indent=2, default=lambda x: x.__dict__))
-    # assert(len(config.ports) == 2)
-    # assert(len(config.flows) == 2)
-    pass
-
-def test_device_traffic():
-    # ipv41 = Ipv4Device('West Ip', address='10.0.0.1', prefix=16, gateway='10.0.1.1')
-    # eth1 = EthernetDevice('West Eth', children=[ipv41], mac='000000000001')
-    # dev1 = Device('East Devices', sessions_per_port=1, protocols=[eth1])
-    # ipv42 = Ipv4Device('East Ip', address='10.0.1.1', prefix=16, gateway='10.0.0.1')
-    # eth2 = EthernetDevice('East Eth', children=[ipv42], mac='000000000002')
-    # ethernet = Ethernet()
-    # device = Device(name='Device1', parent=None, sessions_per_port=10, 
-    #     protocols=[ethernet, vlan, ipv4])
-    # tx = Port(name='Tx Port', location='1.1.1.1/1/1')
-    # rx = Port(name='Rx Port', location='1.1.1.1/1/2')
-    # device_group = DeviceGroup(name='DeviceGroup1', ports=[tx], 
-    #     devices=[device])
-    pass
+def test_port_traffic(serializer):
+    location = Location(Physical(address='127.0.0.1', board=1, port=1))
+    tx = Port(name='Tx', location=location)
+    ipv4 = Ipv4(src=StringPattern('1.1.1.1'), 
+        dst=StringPattern(StringCounter(start='1.1.2.1', step='0.0.0.1', direction='increment', count=10)), 
+        priority=Priority(Dscp(phb=NumberPattern(Dscp.PHB_EF46), ecn=NumberPattern(Dscp.ECN_CAPABLE_TRANSPORT_1))))
+    background = Flow(name='Background Traffic', 
+        endpoint=Endpoint(PortEndpoint(tx_port=tx.name)),
+        packet=[Header(Ethernet()), Header(Vlan()), Header(ipv4)],
+        size=Size(512),
+        rate=Rate(unit='pps', value=1000000))
+    pfc = Flow(name='Pfc Traffic', 
+        endpoint=Endpoint(PortEndpoint(tx_port=tx.name)),
+        packet=[Header(PfcPause())],
+        size=Size(64),
+        rate=Rate(unit='pps', value=1000000))
+    config = Config(state='CREATE', ports=[tx], flows=[background, pfc])
+    print(serializer.json(config))
 
 
 if __name__ == '__main__':
