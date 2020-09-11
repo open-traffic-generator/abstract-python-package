bc9592031f4798eec612f684536e8af211b28e4c
Andy Balogh <andy.balogh@keysight.com>
2020-09-11 08:05:11 -0700

    add config options test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,9 +1,15 @@
 import pytest
-from abstract_open_traffic_generator.config import Config
+from abstract_open_traffic_generator.config import Config, Options
+from abstract_open_traffic_generator.port import Options as PortOptions
 
 
 def test_config(serializer, tx, rx, port_ipv4_traffic):
     config = Config(ports=[tx], flows=[port_ipv4_traffic])
+    print(serializer.json(config))
+
+def test_config_optioins(serializer):
+    options = Options(PortOptions(location_preemption=True))
+    config = Config(options=options)
     print(serializer.json(config))
 
 
