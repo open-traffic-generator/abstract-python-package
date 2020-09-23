0002e6704e31e9c8359b8cbd6fbc9d7ece170c58
Andy Balogh <andy.balogh@keysight.com>
2020-09-23 10:08:38 -0700

    fix build

--- builder.py
+++ builder.py
@@ -407,6 +407,6 @@ class Builder(object):
                         
 
 if __name__ == '__main__':
-    builder = Builder(dependencies=False, clone_and_build=False)
+    builder = Builder(dependencies=True, clone_and_build=True)
     builder.generate().test()
 
